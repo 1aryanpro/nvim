9a7fceea7bfde00732238e5ededb183f02b1fa82
SmashMineGame <aryanpro@icloud.com>
2021-12-25 20:38:46 -0800

    moved lsp mappings to Lua

---
+++ lua/lspConfig/mappings.lua
@@ -0,0 +1,19 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+local function nnoremap(from, to)
+  keymap("n", from, to, opts)
+end
+
+nnoremap('gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+nnoremap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+nnoremap('gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+nnoremap('gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+nnoremap('gl', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>')
+nnoremap('K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+
+nnoremap('<leader>lk', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
+nnoremap('<leader>lp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+nnoremap('<leader>ln', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+nnoremap('<leader>lr', '<cmd>lua vim.lsp.buf.rename()<CR>')
+nnoremap('<leader>la', '<cmd>lua vim.lsp.buf.code_action()<CR>')
