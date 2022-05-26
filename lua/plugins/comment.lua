a5c6d9a31bac470860b1caefa4ea3709aa3caae8
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-05-26 15:07:04 -0700

    switched to default comment keybinds

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,4 +1,4 @@
-local comment  = require 'Comment'
+local comment    = require 'Comment'
 local treesitter = require 'nvim-treesitter.configs'
 
 treesitter.setup {
@@ -10,28 +10,7 @@ treesitter.setup {
 
 comment.setup {
   ignore = '^$',
-  toggler = {
-    line = '<leader>//',
-    block = '<leader>??'
-  },
-  opleader = {
-    line = '<leader>/',
-    block = '<leader>?'
-  },
-  pre_hook = function(ctx)
-    local CU = require 'Comment.utils'
-    local TU = require 'ts_context_commentstring.utils'
-
-    local location = nil
-    if ctx.ctype == CU.ctype.block then
-      location = TU.get_cursor_location()
-    elseif ctx.cmotion == CU.cmotion.v or ctx.cmotion == CU.cmotion.V then
-      location = TU.get_visual_start_location()
-    end
-
-    return require('ts_context_commentstring.internal').calculate_commentstring {
-      key = ctx.ctype == CU.ctype.line and '__default' or '__multiline',
-      location = location,
-    }
-  end,
+  mappings = {
+    extra = true,
+  }
 }
