c1699a3278d975c704ac2ab8bcb194643b04a563
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2023-01-21 21:24:16 -0500

    Formatting Fixes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,28 +1,28 @@
-require'lualine'.setup {
+require 'lualine'.setup {
   options = {
     theme = 'auto',
-    disabled_filetypes = {'which_key'},
-    section_separators = {left = '▌', right = '▐'},
+    disabled_filetypes = { 'which_key' },
+    section_separators = { left = '▌', right = '▐' },
     component_separators = ''
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
     lualine_c = {
       'filename',
       {'GitAdded',   color = {fg = '#98c379'}},
       {'GitEdited',  color = {fg = '#61afef'}},
       {'GitRemoved', color = {fg = '#e86671'}}
     },
-    lualine_x = {'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_x = { 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
