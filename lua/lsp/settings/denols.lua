bf1896e6e14914137625cf0ec0e720385d1aaadc
Aryan Prodduturi <aryanpro@icloud.com>
2023-12-11 23:57:53 -0500

    Updated JS LSPs

---
+++ lua/lsp/settings/denols.lua
@@ -0,0 +1,5 @@
+local opts = {
+  root_dir = require 'lspconfig'.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+return opts
---
+++ lua/lsp/settings/tsserver.lua
@@ -0,0 +1,10 @@
+local opts = {
+  root_dir = require 'lspconfig'.util.root_pattern("package.json"),
+  settings = {
+    format = {
+      enable = false
+    }
+  }
+}
+
+return opts
